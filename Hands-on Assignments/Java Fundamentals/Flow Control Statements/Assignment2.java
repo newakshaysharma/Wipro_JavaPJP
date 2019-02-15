49f4d48a3f56ccaca54648f97148077f9733794f
Manpreet Singh <mpschahal16@gmail.com>
2019-02-15 23:32:31 +0530

    Java Fundamentals/Flow Control Statements/Assignment2

---
+++ Hands-on Assignments/Java Fundamentals/Flow Control Statements/Assignment2.java
@@ -0,0 +1,11 @@
+public class Assignment2 {
+
+	public static void main(String[] args) {
+		int num=Integer.parseInt(args[0]);
+		if(num%2==0)
+			System.out.println("Even");
+		else
+			System.out.println("Odd");
+	}
+
+}
