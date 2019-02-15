d9ed76f3ec65ce583a4b179acb2dd9737b666e43
Manpreet Singh <mpschahal16@gmail.com>
2019-02-15 20:00:02 +0530

    Java Fundamentals/Language Basics/Assignment3

---
+++ Hands-on Assignments/Java Fundamentals/Language Basics/Assignment3.java
@@ -0,0 +1,10 @@
+public class Assignment3 {
+
+	public static void main(String[] args) {
+		int n1=Integer.parseInt(args[0]);
+		int n2=Integer.parseInt(args[1]);
+		int sum=n1+n2;
+		System.out.println("The sum of "+n1+" and "+n2+" is "+sum);
+	}
+
+}
