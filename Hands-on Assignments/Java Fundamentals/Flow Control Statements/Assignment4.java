2bbb9a1df7a7e5dcf98e65cffc001864cf684f96
Manpreet Singh <mpschahal16@gmail.com>
2019-02-15 23:45:09 +0530

    Java Fundamentals/Flow Control Statements/Assignment4

---
+++ Hands-on Assignments/Java Fundamentals/Flow Control Statements/Assignment4.java
@@ -0,0 +1,12 @@
+public class Assignment4 {
+
+	public static void main(String[] args) {
+		char alpha1='e';
+		char alpha2='s';
+		if(alpha1>alpha2)
+			System.out.println(alpha2+","+alpha1);
+		else
+			System.out.println(alpha1+","+alpha2);
+	}
+
+}
