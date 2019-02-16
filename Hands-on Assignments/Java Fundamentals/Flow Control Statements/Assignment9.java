0a1376f18613fde5fc8d6ecfee28da3ee597d094
Manpreet Singh <mpschahal16@gmail.com>
2019-02-17 01:53:16 +0530

    Java Fundamentals/Flow Control Statements/Assignment9

---
+++ Hands-on Assignments/Java Fundamentals/Flow Control Statements/Assignment9.java
@@ -0,0 +1,63 @@
+public class Assignment9 {
+
+	public static void main(String[] args) {
+		if(args.length!=1)
+		{
+			System.out.println("Please enter the month in numbers");
+		}
+		else
+		{
+			String temp = args[0];
+			boolean isNumeric = temp.chars().allMatch( Character::isDigit);
+			if(isNumeric)
+			{
+				int n=Integer.parseInt(args[0]);
+				switch(n) {
+					case 1:
+						System.out.println("January");
+						break;
+					case 2:
+						System.out.println("February");
+						break;
+					case 3:
+						System.out.println("March");
+						break;
+					case 4:
+						System.out.println("April");
+						break;
+					case 5:
+						System.out.println("May");
+						break;
+					case 6:
+						System.out.println("June");
+						break;
+					case 7:
+						System.out.println("July");
+						break;
+					case 8:
+						System.out.println("August");
+						break;
+					case 9:
+						System.out.println("September");
+						break;
+					case 10:
+						System.out.println("October");
+						break;
+					case 11:
+						System.out.println("November");
+						break;
+					case 12:
+						System.out.println("December");
+						break;
+					default:
+						System.out.println("Invalid month");
+						break;
+				}
+			}
+			else
+			{
+				System.out.println("Please enter the month in numbers");
+			}
+		}
+	}
+}
