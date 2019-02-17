62cac4a5119b4963250fbf496e8fd2c738c6715a
Manpreet Singh <mpschahal16@gmail.com>
2019-02-18 01:32:17 +0530

    Java Fundamentals/Arrays/Assignment3 optimise logic

--- Hands-on Assignments/Java Fundamentals/Arrays/Assignment3.java
+++ Hands-on Assignments/Java Fundamentals/Arrays/Assignment3.java
@@ -3,15 +3,13 @@ public class Assignment3 {
 	public static void main(String[] args) {
 		int[] arr= {1,4,34,56,7};
 		int num=Integer.parseInt(args[0]);
-		boolean temp=false;
-		int index=0;
+		int index=-1;
 		for(int i=0;i<arr.length;i++) {
 			if(arr[i]==num) {
-				temp=true;
 				index=i;
 			}
 		}
-		if(temp)
+		if(index!=-1)
 			System.out.println(index);
 		else
 			System.out.println("-1");
