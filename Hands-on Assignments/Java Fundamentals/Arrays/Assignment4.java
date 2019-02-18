e007abda06ae9d606fb8c20228bc341f0f00c42f
Manpreet Singh <mpschahal16@gmail.com>
2019-02-19 00:57:58 +0530

    Java Fundamentals/Arrays/Assignment4

---
+++ Hands-on Assignments/Java Fundamentals/Arrays/Assignment4.java
@@ -0,0 +1,9 @@
+public class Assignment4 {
+
+	public static void main(String[] args) {
+		int[] arr= {65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90};
+		for(int i:arr)
+			System.out.print((char)i);
+	}
+
+}
