978e5e4b0d158b1e9f6bcb601419d609323659ba
Manpreet Singh <mpschahal16@gmail.com>
2019-02-19 01:17:21 +0530

    Java Fundamentals/Arrays/Assignment6

---
+++ Hands-on Assignments/Java Fundamentals/Arrays/Assignment6.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+public class Assignment6{
+
+	public static void main(String[] args) {
+		int[] arr= {1,8,2,9,5,4,6,10,0};
+		Arrays.sort(arr);
+		for(int i:arr)
+		{
+			System.out.print(i+"\t");
+		}
+	}
+
+}
