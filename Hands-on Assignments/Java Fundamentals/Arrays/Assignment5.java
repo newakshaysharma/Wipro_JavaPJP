2e64affd219d16f01c2ec1ebfc3fff060ef0919b
Manpreet Singh <mpschahal16@gmail.com>
2019-02-22 02:09:40 +0530

    Java Fundamentals/Arrays/Assignment5 add remaining find 2 smallest part.

--- Hands-on Assignments/Java Fundamentals/Arrays/Assignment5.java
+++ Hands-on Assignments/Java Fundamentals/Arrays/Assignment5.java
@@ -4,6 +4,8 @@ public class Assignment5 {
 		int[] arr= {1,8,2,9,5,4,6,10,0};
 		int maxOne = 0;
         int maxTwo = 0;
+		int minOne=Integer.MAX_VALUE;
+		int minTwo=Integer.MAX_VALUE;
         for(int n:arr){
             if(maxOne < n){
                 maxTwo = maxOne;
@@ -11,9 +13,17 @@ public class Assignment5 {
             } else if(maxTwo < n){
                 maxTwo = n;
             }
+			
+			if (n< minOne) 
+            { 
+                minTwo = minOne; 
+                 minOne = n; 
+            } 
+            else if (n < minTwo && n != minOne) 
+                minTwo = n; 
         }
-		System.out.println("First Max Number: "+maxOne);
-        System.out.println("Second Max Number: "+maxTwo);
+		System.out.println("Largest 2 numbers are "+maxOne+" & "+maxTwo);
+		System.out.println("Smallest 2 numbers are "+minOne+" & "+minTwo);
 	}
 
 }
