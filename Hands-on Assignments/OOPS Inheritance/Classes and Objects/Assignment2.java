7c0e40d8806f76ad9fefeb50e81e2fa73c518dbd
Manpreet Singh <mpschahal16@gmail.com>
2019-02-22 02:36:40 +0530

    OOPS Inheritance/Classes and Objects/Assignment2.java

---
+++ Hands-on Assignments/OOPS Inheritance/Classes and Objects/Assignment2.java
@@ -0,0 +1,18 @@
+class Calculator {
+	public static int powerInt(int num1, int num2) {
+		return (int) Math.pow(num1, num2);
+	}
+	
+	public static double powerDouble(double num1, int num2) {
+		return Math.pow(num1, num2);
+	}
+}
+
+public class Assignment2 {
+
+	public static void main(String[] args) {
+		System.out.println(Calculator.powerInt(2, 2));
+		System.out.println(Calculator.powerDouble(2.5, 2));
+	}
+
+}
