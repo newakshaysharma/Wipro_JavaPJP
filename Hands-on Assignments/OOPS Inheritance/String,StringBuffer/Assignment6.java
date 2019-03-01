dc49e5bc321adf82ced753ea332d6b8c2d781fec
Manpreet Singh <mpschahal16@gmail.com>
2019-03-01 22:09:49 +0530

    OOPS Inheritance/String,StringBuffer/Assignment6

---
+++ Hands-on Assignments/OOPS Inheritance/String,StringBuffer/Assignment6.java
@@ -0,0 +1,13 @@
+public class Assignment6 {
+	
+	public static void main(String[] args)
+	{
+		String a="hi";
+		String b="hello";
+		String op;
+		if(a.length()<b.length()) op=a+b+a;
+		else op=b+a+b;
+		System.out.println(op);
+		
+	}
+}
