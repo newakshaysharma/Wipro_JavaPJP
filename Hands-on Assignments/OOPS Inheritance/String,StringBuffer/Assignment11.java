0e262138ea00e9badf5c789f7066e04206cefca3
Manpreet Singh <mpschahal16@gmail.com>
2019-03-04 01:59:41 +0530

    OOPS Inheritance/String,StringBuffer/Assignment11

---
+++ Hands-on Assignments/OOPS Inheritance/String,StringBuffer/Assignment11.java
@@ -0,0 +1,42 @@
+public class Assignment11 {
+
+	public static void main(String[] args) {
+		
+		if(args.length!=2)
+		{
+			System.out.println("Plesase pass 2 sring by command line");
+			return;
+		}
+		String str1=args[0];
+		String str2=args[1];
+		boolean isPresent=false;
+		StringBuffer sb=new StringBuffer();
+		
+		while(str1.indexOf(str2)!=-1)
+		{
+			isPresent=true;
+			int index = str1.indexOf(str2);
+			String left=str1.replaceFirst(str2, "");
+			
+		
+			if(index!=0)
+			{
+				sb.append(str1.charAt(index-1));
+			}
+			if(index+str2.length()<str1.length())
+			{
+				sb.append(str1.charAt(index+str2.length()));
+			}
+			
+			
+			str1=left;
+			
+		}
+		
+		if(isPresent) System.out.println(sb);
+		else System.out.println("String b not present in string a");
+		
+		
+	}
+
+}
