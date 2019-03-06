e5f2b5d1ad70689f82a1b948f8ee2741882b2094
Manpreet Singh <mpschahal16@gmail.com>
2019-03-07 00:26:12 +0530

    Hands-on Assignments/Abstraction-Packages-Exception Handling/Packages/ Assignment 1

---
+++ Hands-on Assignments/Abstraction-Packages-Exception Handling/Packages/src/Assignment1/Assignment1.java
@@ -0,0 +1,11 @@
+package Assignment1;
+import testPackage.Foundation;
+public class Assignment1 {
+	
+	public static void main(String[] args){
+		Foundation f=new Foundation();
+		f.var4=16;
+		System.out.println(f.var4);
+	}
+
+}
---
+++ Hands-on Assignments/Abstraction-Packages-Exception Handling/Packages/src/testPackage/Foundation.java
@@ -0,0 +1,8 @@
+package testPackage;
+
+public class Foundation {
+	private int var1;
+	int var2;
+	protected int var3;
+	public int var4;
+}
