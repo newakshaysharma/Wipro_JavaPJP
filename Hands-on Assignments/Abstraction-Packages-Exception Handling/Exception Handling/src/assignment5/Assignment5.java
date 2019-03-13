6b57915a87774445f2603daba6715d49bc26ad38
Manpreet Singh <mpschahal16@gmail.com>
2019-03-13 21:21:51 +0530

    Hands-on Assignments/Abstraction-Packages-Exception Handling/Exception Handling/Assignment5

---
+++ Hands-on Assignments/Abstraction-Packages-Exception Handling/Exception Handling/src/assignment5/Assignment5.java
@@ -0,0 +1,22 @@
+package assignment5;
+
+import java.util.Scanner;
+
+public class Assignment5 {
+	
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter value of a and b.");
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		try{
+			System.out.println("a/b = "+division(a,b));
+		}catch(ArithmeticException ae){
+			System.out.println(ae.getMessage());
+		}
+	}
+	
+	static double division(int a,int b)throws ArithmeticException{
+		return a/b;
+	}
+}
