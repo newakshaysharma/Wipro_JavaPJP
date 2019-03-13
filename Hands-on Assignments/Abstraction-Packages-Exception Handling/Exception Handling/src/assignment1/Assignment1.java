c955fab43f71ee0dd2a3ab5cde395d5266d5e4ff
Manpreet Singh <mpschahal16@gmail.com>
2019-03-13 18:34:32 +0530

    Hands-on Assignments/Abstraction-Packages-Exception Handling/Exception Handling/Assignmet1

---
+++ Hands-on Assignments/Abstraction-Packages-Exception Handling/Exception Handling/src/assignment1/Assignment1.java
@@ -0,0 +1,22 @@
+package assignment1;
+
+import java.util.Scanner;
+
+public class Assignment1 {
+
+	public static void main(String[] args)
+	{
+		String s;
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter an integer: ");
+		s=sc.nextLine();
+		try{
+			int i=Integer.parseInt(s);
+			System.out.println("The square value is "+(i*i));
+			System.out.println("The work has been done successfully.");
+		}catch(Exception e){
+			System.out.println("Entered input is not a valid format for an integer.");
+		}
+		sc.close();
+	}
+}
