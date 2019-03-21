b4eb890c4d90b2835203e8a7bd60bc74d1e00201
Manpreet Singh <mpschahal16@gmail.com>
2019-03-22 03:05:36 +0530

    Hands-on Assignments/Junit/Test Suite/ update with 2 more testcases

--- Hands-on Assignments/Junit/Test Suite/src/topic2/src/Test1.java
+++ Hands-on Assignments/Junit/Test Suite/src/topic2/src/Test1.java
@@ -12,6 +12,7 @@ public class Test1 {
 	{
 		Demo1 d1=new Demo1();
 		assertEquals("Result","Manpreet Singh",d1.stringConcat("Manpreet ", "Singh"));
+		assertEquals("Result","ManpreetSingh",d1.stringConcat("Manpreet", "Singh"));
 	}
 
 }
