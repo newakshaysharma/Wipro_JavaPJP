69c8d9b87410ef245c3bf31efb5747303fb0cc5f
Manpreet Singh <mpschahal16@gmail.com>
2019-03-22 02:00:08 +0530

    Hands-on Assignments/Junit/Assert methods and annotations/assignment1

---
+++ Hands-on Assignments/Junit/Assert methods and annotations/src/assignment1/Employee.java
@@ -0,0 +1,17 @@
+package assignment1;
+
+import java.util.ArrayList;
+
+public class Employee {
+	
+	public String findName(ArrayList<String> employees,String name){
+		  String result="";
+		  if(employees.contains(name)){
+		   result="FOUND";
+		  }else{
+		   result="NOT FOUND";
+		  }
+		  return result;
+		 }
+
+}
---
+++ Hands-on Assignments/Junit/Assert methods and annotations/src/assignment1/EmployeeTest.java
@@ -0,0 +1,29 @@
+package assignment1;
+
+import java.util.ArrayList;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class EmployeeTest {
+	
+	Employee e1;
+	ArrayList<String> namelist;
+	
+	@Before
+	public void before()
+	{
+		e1=new Employee();
+		namelist=new ArrayList<String>();
+		namelist.add("Manpreet");
+		namelist.add("Singh");
+		namelist.add("Jaspreet");
+		
+	}
+	
+	@Test
+	public void testEmployee(){
+		assertEquals("Result","FOUND",e1.findName(namelist,"Singh"));
+	}
+
+}
