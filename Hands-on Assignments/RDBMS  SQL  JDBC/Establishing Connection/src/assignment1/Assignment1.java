ec9898c6fa9e533a17f61eab13b79e53c27fcc90
Manpreet Singh <mpschahal16@gmail.com>
2019-04-29 02:57:20 +0530

    Hands-on Assignments/RDBMS  SQL  JDBC/Establishing Connection/src/assignment1/ rearrange

--- Hands-on Assignments/RDBMS  SQL  JDBC/Establishing Connection/src/assignment1/Assignment1.java
+++ Hands-on Assignments/RDBMS  SQL  JDBC/Establishing Connection/src/assignment1/Assignment1.java
@@ -12,10 +12,11 @@ public class Assignment1 {
 			try {			
 				Class.forName("oracle.jdbc.driver.OracleDriver");
 				conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "hr", "hr");
+	
 				System.out.println("Connection Established successfully");
 			} catch (Exception e) {
-				e.printStackTrace();
 				System.out.println("Connection could  not be established");
+				e.printStackTrace();
 			} finally {
 				if (conn != null) conn.close();
 			}
