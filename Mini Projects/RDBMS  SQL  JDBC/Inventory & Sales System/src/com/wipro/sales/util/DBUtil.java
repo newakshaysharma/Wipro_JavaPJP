dab9f2a92fc919b2c6fa52340569abd99a15888c
Manpreet Singh <mpschahal16@gmail.com>
2019-05-02 19:01:53 +0530

    Mini Projects/RDBMS  SQL  JDBC/Inventory & Sales System/src/com/wipro/sales/util/

---
+++ Mini Projects/RDBMS  SQL  JDBC/Inventory & Sales System/src/com/wipro/sales/util/DBUtil.java
@@ -0,0 +1,32 @@
+package com.wipro.sales.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBUtil {
+
+	private static Connection conn;
+	public static Connection getConnection() {
+
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "hr", "hr");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return conn;
+	}
+
+	public static void closeConnection() {
+			try {
+				if(!conn.isClosed())
+					conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+	}
+}
